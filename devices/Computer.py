10b681640f581e4ca778c7832826edaac1be42ca
Maxime Bergeron <mazout360@gmail.com>
2019-09-23 16:05:14 -0400

    [COMPUTER] Add linux computer device

--- devices/Computer.py
+++ devices/Computer.py
@@ -1,39 +1,42 @@
 #!/usr/bin/env python3
 '''
-    File name: Milight.py
+    File name: Computer.py
     Author: Maxime Bergeron
-    Date last modified: 30/07/2019
-    Python Version: 3.7
+    Date last modified: 23/09/2019
+    Python Version: 3.5
 
-    A generic bash function On/Off device handler class 
+    A specialized GenericOnOff for linux computer devices.
 '''
 
 import subprocess
-import time
 from devices.common import *
 from devices.device import device
 
-class GenericOnOff(device):
+class Computer(device):
     def __init__(self, devid, config):
         super().__init__(devid, config)
         self.config = config["DEVICE"+str(devid)]
         self.device = self.config["DEVICE"]
-        self.device_type = "GenericOnOff"
+        self.device_type = "Computer"
         self.color_type = "io-ops"
-        debug.write("Created generic On/Off device named: {}".format(self.device), 0, self.device_type)
+        self.ip = self.config["IP_ADDRESS"]
+        self.mac = self.config["MAC_ADDRESS"]
+        self.user = self.config["SSH_USER"]
+        self.device_type = "Computer"
+        debug.write("Created computer device named: {}".format(self.device), 0, self.device_type)
 
     def get_state(self):
         if self.action_delay != 0 and self.last_action_timestamp + self.action_delay > int(time.time()):
             self.state = DEVICE_STANDBY
             return self.state
-        if self.config["STATE"] and not self.success:
+        if not self.success:
             try:
-                _stdout = subprocess.check_output(self.config["STATE"], 
+                _stdout = subprocess.check_output("ping {} -c 1 -W 1".format(self.ip), 
                                                   shell=True).decode('UTF-8')
             except subprocess.CalledProcessError:
                 self.state = 0
                 return 0
-            if self.config["STATE_ON_EXPECT"] in _stdout:
+            if "1 received" in _stdout:
                 self.state = 1
                 return 1
             self.state = 0
@@ -41,18 +44,25 @@ class GenericOnOff(device):
         return self.state
         
     def run(self, color, priority):
-        if color == DEVICE_OFF and self.config["OFF"]:
+    	#TODO support windows ?
+        if color == DEVICE_OFF:
             debug.write("Turning device {} OFF".format(self.device), 0, self.device_type)
-            os.system(self.config["OFF"])
+            os.system("ssh {}@{} 'sudo shutdown now'".format(self.user, self.ip))
             self.success = True
             self.state = 0
             return True
-        elif color == DEVICE_ON and self.config["ON"]:
+        elif color == DEVICE_ON:
             debug.write("Turning device {} ON".format(self.device), 0, self.device_type)
-            os.system(self.config["ON"])
+            os.system("/usr/bin/wakeonlan {}".format(self.mac))
+            self.success = True
+            self.state = 1
+            return True
+        elif color == "2":
+            debug.write("Restarting device {}".format(self.device), 0, self.device_type)
+            os.system("ssh {}@{} 'sudo reboot'".format(self.user, self.ip))
             self.success = True
             self.state = 1
             return True
         debug.write("Request for state {} cannot be handled for device {}".format(color, self.device), 1, self.device_type)
         self.success = True
-        return True        
+        return True   
